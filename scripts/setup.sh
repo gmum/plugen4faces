e85d1789049c5b802c196284f3d28419bb21b6a4
Adrian Suwała <63507293+adriansuwala@users.noreply.github.com>
2024-02-28 22:10:53 +0100

    Updates setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,3 @@ conda env config vars set CUDA_HOME=$CONDA_PREFIX -n $NAME
 conda env config vars set CPATH=$CONDA_PREFIX/include:$CPATH -n $NAME
 conda deactivate && conda activate $NAME
 LDFLAGS="-I$CONDA_PREFIX/lib -I$CONDA_PREFIX/lib64" pip --no-input install dlib==19.24.0 --verbose
-python -c "from plugen4faces.flow import SimpleRealNVP"
