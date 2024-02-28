80d3a5191ea4f0f9bfdaf2f0992c7673b9527c75
Adrian Suwała <63507293+adriansuwala@users.noreply.github.com>
2024-02-28 22:19:34 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+   name="plugen4faces",
+   version="1.0",
+   description="Official code for PluGeN4Faces, a method for explicitly disentangling attributes from person's identity. This work was presented at WACV 2024.",
+)
